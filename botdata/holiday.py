faedcc35ffab79ed2addbde958aebb57cc79d4fd
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2023-03-20 16:52:45 +0700

    doc: docstring

--- botdata/holiday.py
+++ botdata/holiday.py
@@ -5,6 +5,14 @@ import pandas as pd
 
 
 def get_holidays(year: int) -> List[date]:
+    """Get holidays from Bank of Thailand website
+
+    Args:
+        year (int): year
+
+    Returns:
+        List[date]: list of holidays
+    """
     df = pd.read_html(
         f"https://www.bot.or.th/English/FinancialInstitutions/FIholiday/Pages/{year}.aspx"
     )[0]
@@ -21,10 +29,26 @@ def get_holidays(year: int) -> List[date]:
 
 
 def is_holiday(date_: date) -> bool:
+    """Check if date is holiday
+
+    Args:
+        date_ (date): date
+
+    Returns:
+        bool: True if date is holiday
+    """
     return date_ in get_holidays(date_.year)
 
 
 def is_business_day(date_: date) -> bool:
+    """Check if date is business day
+
+    Args:
+        date_ (date): date
+
+    Returns:
+        bool: True if date is business day
+    """
     return date_.weekday() < 5 and not is_holiday(date_)
 
 

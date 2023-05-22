f0f15b15149de94ae27080e9d29e0ad1d6958cbf
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2023-05-22 12:06:07 +0700

    doc: example

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ from datetime import date
 import botdata as bd
 
 # Get holidays in 2020
-bd.get_holidays(2020)
+bd.get_holidays(2022)
 
 # Check if date is business day
 bd.is_business_day(date.today())
--- example/example.py
+++ example/example.py
@@ -3,10 +3,10 @@ from datetime import date
 import botdata as bd
 
 # Get holidays in 2020
-bd.get_holidays(2020)
+bd.get_holidays(2022)
 
-# Check if date is holiday
-bd.is_business_day(date(2020, 1, 1))
+# Check if date is business day
+bd.is_business_day(date.today())
 
 # Get next business day
-bd.next_business_day(date(2020, 1, 1), 1)
+bd.next_business_day(date.today(), 1)
